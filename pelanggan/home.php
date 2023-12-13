ba0c2631ab78172d9187387a2160c06ca499aa04
Ilham Syarief <115606564+ilhamsyarief21@users.noreply.github.com>
2023-12-13 17:30:45 +0700

    update fitur maksimal 2x sewa

--- pelanggan/home.php
+++ pelanggan/home.php
@@ -1,112 +1,99 @@
-<!-- Owl Carousel CSS -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Owl Carousel CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+    <!-- Fancybox CSS -->
+    <link rel="stylesheet" href="path/to/fancybox.css">
 
-<!-- Owl Carousel JavaScript -->
-<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-<style>
-</style>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <!-- Owl Carousel JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <!-- Fancybox JavaScript -->
+    <script src="path/to/jquery.fancybox.min.js"></script>
+</head>
+<body>
+    <!-- Owl Carousel -->
+    <div class="owl-carousel">
+        <div class="item"><img src="assets/img/a.png" alt="Slide 1" style="width: 100%; height: auto;"></div>
+        <div class="item"><img src="assets/img/b.png" alt="Slide 2" style="width: 100%; height: auto;"></div>
+        <div class="item"><img src="assets/img/c.png" alt="Slide 3" style="width: 100%; height: auto;"></div>
+        <div class="item"><img src="assets/img/d.png" alt="Slide 4" style="width: 100%; height: auto;"></div>
+        <div class="item"><img src="assets/img/e.png" alt="Slide 5" style="width: 100%; height: auto;"></div>
+    </div>
 
-<div class="owl-carousel">
-    <div class="item"><img src="assets/img/a.png" alt="Slide 1" style="width: 100%; height: auto;"></div>
-    <div class="item"><img src="assets/img/b.png" alt="Slide 2" style="width: 100%; height: auto;"></div>
-    <div class="item"><img src="assets/img/c.png" alt="Slide 3" style="width: 100%; height: auto;"></div>
-    <div class="item"><img src="assets/img/d.png" alt="Slide 4" style="width: 100%; height: auto;"></div>
-    <div class="item"><img src="assets/img/e.png" alt="Slide 5" style="width: 100%; height: auto;"></div>
-</div>
-
-
-<!-- Baris Kamear -->
-<div class="row">
-    <?php
-    $query = $connection->query("SELECT * FROM kameraku JOIN jenis USING(id_jenis)");
-    while ($row = $query->fetch_assoc()):
-        // Cek apakah pelanggan telah melebihi batas transaksi
-        $idPelanggan = isset($_SESSION['id_pelanggan']) ? $_SESSION['id_pelanggan'] : null;
-        $batasTransaksi = 5; // Tetapkan batas transaksi
-        $jumlahTransaksiPelanggan = hitungJumlahTransaksiPelanggan($connection, $idPelanggan);
-
-        // Nonaktifkan tombol jika pelanggan telah melebihi batas transaksi
-        $isNonaktif = ($jumlahTransaksiPelanggan >= $batasTransaksi) ? "disabled" : "";
-    ?>
-        <div class="col-xs-6 col-md-3">
-            <!-- Kamera Thumbnail -->
-            <div class="thumbnail">
-                <a href="assets/img/kamera/<?=$row['gambar']?>" class="fancybox">
-                    <img src="assets/img/kamera/<?=$row['gambar']?>" alt="<?=$row['nama_kamera']?>" style="height: 200px;">
-                </a>
-                <div class="caption text-center">
-                    <!-- Informasi Kamera -->
-                    <h4><?=$row["nama_kamera"]?></h4>
-                    <h5>Rp.<?=$row["harga"]?>,- <?=$row["nama"]?> - <?=$row["merk"]?></h5>
-                    <h6><?=$row["no_kamera"]?></h6>
-                    <span class="label label-<?=($row['status']) ? "success" : "danger" ?>">
-                        <?=($row['status']) ? "Tersedia" : "Tidak Tersedia" ?>
-                    </span>
-                    <p>
-                        <!-- Tombol Sewa -->
-                        <br>
-                        <a href="<?=($row['status'] && !$isNonaktif) ? "?page=transaksi&id=$row[id_kameraku]" : "#" ?>" class="btn btn-primary" <?=($row['status'] && !$isNonaktif) ? "" : "disabled" ?>>Sewa Sekarang!</a>
-                    </p>
+     <!-- Baris Kamera -->
+     <div class="row">
+        <?php
+        $batasTransaksi = 2; // Tetapkan batas transaksi
+        // Sambungkan ke database dan ambil data kamera
+        // Saring data sesuai kebutuhan Anda
+        $query = $connection->query("SELECT * FROM kameraku JOIN jenis USING(id_jenis)");
+        while ($row = $query->fetch_assoc()):
+            // Cek apakah pelanggan telah melebihi batas transaksi
+            $idPelanggan = isset($_SESSION['id_pelanggan']) ? $_SESSION['id_pelanggan'] : null;
+            $queryTransaksi = $connection->prepare("SELECT COUNT(*) as jumlah FROM transaksi WHERE id_pelanggan = ?");
+            $queryTransaksi->bind_param("i", $idPelanggan);
+            $queryTransaksi->execute();
+            $resultTransaksi = $queryTransaksi->get_result();
+            $jumlahTransaksiPelanggan = $resultTransaksi->fetch_assoc()['jumlah'];
+            
+            // Nonaktifkan tombol jika pelanggan telah melebihi batas transaksi
+            $isNonaktif = ($jumlahTransaksiPelanggan >= $batasTransaksi) ? "disabled" : "";
+        ?>
+            <div class="col-xs-6 col-md-3">
+                <!-- Kamera Thumbnail -->
+                <div class="thumbnail">
+                    <a href="assets/img/kamera/<?=$row['gambar']?>" class="fancybox">
+                        <img src="assets/img/kamera/<?=$row['gambar']?>" alt="<?=$row['nama_kamera']?>" style="height: 200px;">
+                    </a>
+                    <div class="caption text-center">
+                        <!-- Informasi Kamera -->
+                        <h4><?=$row["nama_kamera"]?></h4>
+                        <h5>Rp.<?=$row["harga"]?>,- <?=$row["nama"]?> - <?=$row["merk"]?></h5>
+                        <h6><?=$row["no_kamera"]?></h6>
+                        <span class="label label-<?=($row['status']) ? "success" : "danger" ?>">
+                            <?=($row['status']) ? "Tersedia" : "Tidak Tersedia" ?>
+                        </span>
+                        <p>
+                            <!-- Tombol Sewa -->
+                            <br>
+                            <a href="<?=($row['status'] && !$isNonaktif) ? "?page=transaksi&id=$row[id_kameraku]" : "#" ?>" class="btn btn-primary" <?=($row['status'] && !$isNonaktif) ? "" : "disabled" ?>>Sewa Sekarang!</a>
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
-    <?php endwhile; ?>
-</div>
+        <?php endwhile; ?>
+    </div>
 
-<!-- Fancybox Script -->
-<script type="text/javascript">
-    $(document).ready(function(){
-        $(".fancybox").fancybox({
-            openEffect  : 'none',
-            closeEffect : 'none',
-            iframe : {
-                preload: false
-            }
+    <!-- Fancybox Script -->
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $(".fancybox").fancybox({
+                openEffect  : 'none',
+                closeEffect : 'none',
+                iframe : {
+                    preload: false
+                }
+            });
         });
-        $(".various").fancybox({
-            maxWidth    : 800,
-            maxHeight   : 600,
-            fitToView   : false,
-            width       : '70%',
-            height      : '70%',
-            autoSize    : false,
-            closeClick  : false,
-            openEffect  : 'none',
-            closeEffect : 'none'
-        });
-        $('.fancybox-media').fancybox({
-            openEffect  : 'none',
-            closeEffect : 'none',
-            helpers : {
-                media : {}
-            }
-        });
-    });
-</script>
+    </script>
 
-<script type="text/javascript">
-   $(document).ready(function(){
-      $(".owl-carousel").owlCarousel({
-         items: 1,
-         autoplay: true,
-         autoplayTimeout: 5000,
-         loop: true,
-         nav: true,
-         navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
-      });
-   });
-</script>
-
-<?php
-
-function hitungJumlahTransaksiPelanggan($connection, $idPelanggan) {
-    $query = $connection->prepare("SELECT COUNT(*) as jumlah FROM transaksi WHERE id_pelanggan = ?");
-    $query->bind_param("i", $idPelanggan);
-    $query->execute();
-    $result = $query->get_result();
-    $row = $result->fetch_assoc();
-    return $row['jumlah'];
-}
-?>
+    <!-- Owl Carousel Script -->
+    <script type="text/javascript">
+       $(document).ready(function(){
+          $(".owl-carousel").owlCarousel({
+             items: 1,
+             autoplay: true,
+             autoplayTimeout: 5000,
+             loop: true,
+             nav: true,
+             navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
+          });
+       });
+    </script>
+</body>
+</html>
